0d654c8de2543ed5bec7f279138cf33ae958f84c
jsteenb2 <jonathansteenbergen@gmail.com>
2017-11-05 14:58:42 -0600

    add func types run down
    to run, $ go test -v ./company/func_test.go

---
+++ func_test.go
@@ -0,0 +1,69 @@
+package zerosixty_test
+
+import "testing"
+
+/*
+	func types can be used in similar ways to an interface
+	achieve polymorphic behavior without structs w/ methods on it
+*/
+
+type aggregator func(...int) int
+
+func sum(ints ...int) int {
+	sum := 0
+	for _, v := range ints {
+		sum += v
+	}
+	return sum
+}
+
+func max(ints ...int) int {
+	max := 0
+	for _, v := range ints {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func min(ints ...int) int {
+	min := ints[0]
+	for _, v := range ints[1:] {
+		if min > v {
+			min = v
+		}
+	}
+	return min
+}
+
+func count(ints ...int) int {
+	return len(ints)
+}
+
+func avg(ints ...int) int {
+	return sum(ints...) / len(ints)
+}
+
+func TestFuncTypes(t *testing.T) {
+	ints := []int{0, 3, 1, 2, 31, -3, 11}
+
+	// running table tests
+	aggregators := []struct {
+		expected int
+		name     string
+		aggregator
+	}{
+		{45, "sum", sum},
+		{-3, "min", min},
+		{31, "max", max},
+		{7, "count", count},
+		{6, "avg", avg},
+	}
+
+	for _, tc := range aggregators {
+		if output := tc.aggregator(ints...); output == tc.expected {
+			t.Logf("%s:\t%d", tc.name, output)
+		}
+	}
+}
