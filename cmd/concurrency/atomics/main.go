245f148be52004d2c444bed770ca571a90af5133
jsteenb2 <jonathansteenbergen@gmail.com>
2017-11-05 21:31:12 -0600

    add colorization to output

--- cmd/concurrency/atomics/main.go
+++ cmd/concurrency/atomics/main.go
@@ -10,15 +10,21 @@ import (
 	atomic functions are the most performant way to serialize operations on shared memory
 */
 
+const (
+	red   = "\033[0;31m"
+	nc    = "\033[0m"
+	green = "\033[0;32m"
+)
+
 var counter int64
 
 func incCounter(step int) {
 	for i := 0; i < 100; i++ {
 		atomic.AddInt64(&counter, int64(step))
-		if step > 1 {
-			fmt.Println("--->", counter)
+		if step == 1 {
+			fmt.Println(green, counter, nc)
 		} else {
-			fmt.Println(counter)
+			fmt.Println(red, counter, nc)
 		}
 
 	}
@@ -33,7 +39,7 @@ func main() {
 		wg.Done()
 	}()
 	go func() {
-		incCounter(10)
+		incCounter(-2)
 		wg.Done()
 	}()
 
