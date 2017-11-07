a99b49e787c8502042e46a0eafb87084c7fa3d63
jsteenb2 <jonathansteenbergen@gmail.com>
2017-11-06 19:06:01 -0600

    add benchmark tests example
    to run, $ go test -v ./testing/benchmark_test.go

---
+++ testing/benchmark_test.go
@@ -0,0 +1,31 @@
+// go test -run none -bench . -benchtime 3s -benchmem
+package testing_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+var str string
+
+// BenchmarkSprint tests the performance of using Sprint.
+func BenchmarkSprint(b *testing.B) {
+	var s string
+
+	for i := 0; i < b.N; i++ {
+		s = fmt.Sprint("hello")
+	}
+
+	str = s
+}
+
+// BenchmarkSprint tests the performance of using Sprintf.
+func BenchmarkSprintf(b *testing.B) {
+	var s string
+
+	for i := 0; i < b.N; i++ {
+		s = fmt.Sprintf("hello")
+	}
+
+	str = s
+}
